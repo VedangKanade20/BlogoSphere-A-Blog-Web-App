4032b368c595d51b88b721024eba7cd85d09d99a
VedangKanade <vedangk20@gmail.com>
2024-05-05 19:21:13 +0530

    user login functionality in backend done

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,29 +1,34 @@
-import mongoose from 'mongoose';
+import bcrypt from "bcryptjs";
+import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema(
-	{
-		name: {
-			type: String,
-			required: true,
-		},
-		email: {
-			type: String,
-			required: true,
-			unique: true,
-		},
-		password: {
-			type: String,
-			required: true,
-		},
-		isAdmin: {
-			type: Boolean,
-			required: true,
-			default: false,
-		},
-	},
-	{ timestamps: true }
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamps: true }
 );
 
-const User = mongoose.model('User', userSchema);
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
 
 export default User;
