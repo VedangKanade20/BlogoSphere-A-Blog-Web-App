8a2206a38d72837fc8110f5051c2217434498a35
VedangKanade <vedangk20@gmail.com>
2024-05-07 12:43:30 +0530

    /

---
+++ frontend/src/screens/LoginScreen.js
@@ -0,0 +1,17 @@
+/* import { Button } from "@chakra-ui/react";
+import { useDispatch } from "react-redux";
+import {
+  Link as RouterLink,
+  useNavigate,
+  useSearchParams,
+} from "react-router-dom";
+
+const LoginScreen = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [searchParams] = useSearchParams();
+  let redirect = searchParams.get("redirect") || "/";
+}; */
+
+
