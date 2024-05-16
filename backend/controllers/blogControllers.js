3411949fa6cabb6adb279dd7587fcd45f3747a71
VedangKanade <vedangk20@gmail.com>
2024-05-16 16:36:52 +0530

    addblog controller added

--- backend/controllers/blogControllers.js
+++ backend/controllers/blogControllers.js
@@ -1,7 +1,131 @@
+// import asyncHandler from "express-async-handler";
+
+// import Blog from "../models/blogModel.js";
+// import User from "../models/userModel.js";
+
+// /**
+//  * @desc		Get all blogs
+//  * @route		GET /api/blogs
+//  * @access	public
+//  */
+
+// const getBlogs = asyncHandler(async (req, res) => {
+//   const blogs = await Blog.find({});
+//   res.json(blogs);
+// });
+
+// /**
+//  * @desc		Get single blog
+//  * @route		GET /api/blogs/:id
+//  * @access	public
+//  */
+
+// const getBlogById = asyncHandler(async (req, res) => {
+//   try {
+//     const blog = await Blog.findById(req.params.id);
+
+//     if (blog) {
+//       res.json(blog);
+//     } else {
+//       res.status(404).json({ message: "Blog not found" });
+//     }
+//   } catch (err) {
+//     console.log(err.message);
+//     res.json(err);
+//   }
+// });
+
+// /**
+//  * @desc		Create a blogd
+//  * @route		POST /api/blogs/postBlog
+//  * @access	private/admin
+//  */
+// const createBlog = asyncHandler(async (req, res) => {
+//   const { title, content, image } = req.body;
+//   try {
+//     const newBlog = new Blog({
+//       title,
+//       content,
+//       image,
+//       user: req.user._id,
+//     });
+//     const createdBlog = await newBlog.save();
+//     res.status(201).json(createdBlog);
+//   } catch (err) {
+//     console.log(err.message);
+//     res.status(500).json({ message: "Server Error" });
+//   }
+// });
+
+// /**
+//  * @desc		Update a product
+//  * @route		PUT/api/blogs/:id
+//  * @access	private/admin
+//  */
+// const updateBlog = asyncHandler(async (req, res) => {
+//   const { title, content, image } = req.body;
+
+//   const blog = await Blog.findById(req.params.id);
+
+//   if (blog) {
+//     blog.title = title;
+//     blog.content = content;
+//     blog.image = image;
+
+//     const updatedBlog = await blog.save();
+//     res.json(updatedBlog);
+//   } else {
+//     res.status(404);
+//     throw new Error("Blog not Found");
+//   }
+// });
+
+// /**
+//  * @desc		Delete a blog
+//  * @route		DELETE /api/blogs/:id
+//  * @access	private
+//  */
+// const deleteBlog = asyncHandler(async (req, res) => {
+//   const blog = await Blog.findById(req.params.id);
+
+//   if (blog) {
+//     await blog.deleteOne();
+//     res.json({ message: "Blog deleted" });
+//   } else {
+//     res.status(404);
+//     throw new Error("Blog not Found");
+//   }
+// });
+
+// /**
+//  * @desc		Create a new review
+//  * @route		POST /api/products/:id/reviews
+//  * @access	private
+//  */
+// const createBlogReview = asyncHandler(async (req, res) => {
+//   const { rating, comment} = req.body;
+
+//   const blog =  await Blog.findById(req.params.id);
+
+//   if(blog) {
+//     const alreadyRe
+//   }
+// })
+
+
+// export {
+//   getBlogById,
+//   getBlogs,
+//   createBlog,
+//   updateBlog,
+//   deleteBlog,
+//   createBlogReview,
+// };
+
+
 import asyncHandler from "express-async-handler";
 
 import Blog from "../models/blogModel.js";
-import User from "../models/userModel.js";
 
 /**
  * @desc		Get all blogs
@@ -36,10 +160,11 @@ const getBlogById = asyncHandler(async (req, res) => {
 });
 
 /**
- * @desc		Create a blogd
+ * @desc		Create a product
  * @route		POST /api/blogs/postBlog
  * @access	private/admin
  */
+
 const createBlog = asyncHandler(async (req, res) => {
   const { title, content, image } = req.body;
   try {
@@ -62,6 +187,7 @@ const createBlog = asyncHandler(async (req, res) => {
  * @route		PUT/api/blogs/:id
  * @access	private/admin
  */
+
 const updateBlog = asyncHandler(async (req, res) => {
   const { title, content, image } = req.body;
 
@@ -85,6 +211,7 @@ const updateBlog = asyncHandler(async (req, res) => {
  * @route		DELETE /api/blogs/:id
  * @access	private
  */
+
 const deleteBlog = asyncHandler(async (req, res) => {
   const blog = await Blog.findById(req.params.id);
 
@@ -98,26 +225,45 @@ const deleteBlog = asyncHandler(async (req, res) => {
 });
 
 /**
- * @desc		Create a new review
- * @route		POST /api/products/:id/reviews
- * @access	private
+ * @desc    Add review comment and rating to a blog
+ * @route   POST /api/blogs/:id/addReview
+ * @access  private
  */
-const createBlogReview = asyncHandler(async (req, res) => {
-  const { rating, comment} = req.body;
 
-  const blog =  await Blog.findById(req.params.id);
+const addBlogReview = asyncHandler(async (req, res) => {
+  const { comment, rating } = req.body;
+  const blogId = req.params.id;
+  const userId = req.user._id; // Assuming you have user information in the request
 
-  if(blog) {
-    const alreadyRe
+  try {
+    const blog = await Blog.findById(blogId);
+
+    if (!blog) {
+      return res.status(404).json({ message: "Blog not found" });
+    }
+
+    // Add the review to the blog
+    blog.reviews.push({ user: userId, comment, rating });
+    // Recalculate the average rating
+    blog.averageRating = (
+      blog.reviews.reduce((total, review) => total + review.rating, 0) /
+      blog.reviews.length
+    ).toFixed(1); // Round to 1 decimal place
+
+    // Save the updated blog
+    await blog.save();
+    res.status(201).json({ message: "Review added successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
   }
-})
-
+});
 
 export {
+  addBlogReview,
+  createBlog,
+  deleteBlog,
   getBlogById,
   getBlogs,
-  createBlog,
   updateBlog,
-  deleteBlog,
-  createBlogReview,
 };
--- backend/models/blogModel.js
+++ backend/models/blogModel.js
@@ -1,89 +1,21 @@
-// import mongoose from "mongoose";
-
-// const reviewSchema = mongoose.Schema(
-//   {
-
-//     title: {
-//       type: String,
-//       required: true,
-//     },
-//     rating: {
-//       type: Number,
-//       required: true,
-//     },
-//     user: {
-//       type: mongoose.Schema.Types.ObjectId,
-//       ref: "User",
-//       required: true,
-//     },
-//   },
-//   { timestamps: true }
-// );
-
-// const blogSchema = mongoose.Schema(
-//   {
-//     user: {
-//       type: mongoose.Schema.Types.ObjectId,
-//       ref: "User",
-//       required: true,
-//     },
-//     title: {
-//       type: String,
-//       required: true,
-//     },
-//     content: {
-//       type: String,
-//       required: true,
-//     },
-//     image: {
-//       type: String,
-//       required: true,
-//     },
-//     rating: {
-//       type: Number,
-//       required: true,
-//       default: 0,
-//     },
-//     numReviews: {
-//       type: Number,
-//       required: true,
-//       default: 0,
-//     },
-//     reviews: [reviewSchema],
-//   },
-//   { timestamps: true }
-// );
-
-// const Blog = mongoose.model("Blog", blogSchema);
-
-// export default Blog;
-
 import mongoose from "mongoose";
 
 const reviewSchema = mongoose.Schema(
   {
-    comment: [
-      {
+    comment: {
+      user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Blog",
+        ref: "User",
         required: true,
-      },
-    ],
+      }, // Add user field
+      type: String,
+      required: true,
+    },
+
     rating: {
       type: Number,
       required: true,
     },
-
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    /*  blog: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Blog",
-      required: true,
-    }, */
   },
   { timestamps: true }
 );
@@ -107,16 +39,15 @@ const blogSchema = mongoose.Schema(
       type: String,
       required: true,
     },
-    rating: {
+    ratings: {
       type: Number,
       required: true,
-      default: 0,
     },
-    numReviews: {
+    averageRating: {
       type: Number,
-      required: true,
       default: 0,
     },
+
     reviews: [reviewSchema],
   },
   { timestamps: true }
